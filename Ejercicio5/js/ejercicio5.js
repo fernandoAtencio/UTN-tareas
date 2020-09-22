1b497e9360a1c1ff72c5b35bd6ec233c4f95457f
Fernando Atencio <atencio.fernando@gmail.com>
2020-09-22 20:22:46 -0300

    Modifiqué el ejercicio 5 para que cuente los 0 que no son par ni impar

--- Ejercicio5/js/ejercicio5.js
+++ Ejercicio5/js/ejercicio5.js
@@ -4,13 +4,15 @@ let ceros = 0;
 
 for (let i = 1; i <= 5; i++) {
   let numero = parseInt(prompt("Ingrese un número"));
-  if (numero % 2 == 0) {
+  if (numero % 2 == 0 && numero != 0) {
     numerosPares++;
+  } else if (numero === 0) {
+    ceros++;
   } else {
     numerosImpares++;
   }
 }
 
 document.write(
-  `Ingresaste ${numerosPares} números pares. <br> Ingresaste ${numerosImpares} números impares.`
+  `Ingresaste ${numerosPares} números pares. <br> Ingresaste ${numerosImpares} números impares. <br> Ingresaste ${ceros} ceros.`
 );
