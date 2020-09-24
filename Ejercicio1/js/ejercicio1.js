e562915a4272f3b6f6c4062adaa7f9905cf341b8
Fernando Atencio <atencio.fernando@gmail.com>
2020-09-24 10:35:32 -0300

    Agregué otra forma de resolución con For y un operador ternario

--- Ejercicio1/js/ejercicio1.js
+++ Ejercicio1/js/ejercicio1.js
@@ -1,4 +1,4 @@
-const numero1 = Number(prompt("Ingresa un número"));
+/*const numero1 = Number(prompt("Ingresa un número"));
 const numero2 = Number(prompt("Ingresa otro número"));
 const numero3 = Number(prompt("Ingresa otro numero"));
 
@@ -7,3 +7,14 @@ if (numero1 < 10 || numero2 < 10 || numero3 < 10) {
 } else {
   document.write("No ingresaste números menores a 10");
 }
+*/
+const numeros = [];
+
+for (let i = 1; i <= 3; i++) {
+  let numero = Number(prompt(`Ingresá el número ${i}`));
+  numeros.push(numero);
+}
+let maximo = Math.max(...numeros);
+maximo <= 10
+  ? document.write("Alguno de los números es menor a 10")
+  : document.write("No ingresaste números menores a 10");
