f80db41c03981f89e5cc7ea14d640fbd5ad66cc4
Fernando Atencio <atencio.fernando@gmail.com>
2020-10-14 12:03:27 -0300

    Primer clase de Node. Crear rutas y renderizar vistas

---
+++ Node/clase01/routes/alumnos.js
@@ -0,0 +1,30 @@
+const express = require("express");
+// VERBOS HTTP -> GET, POST, PUT , DELETE
+const router = express.Router(); // Detectar verbos http
+const profe = {
+  nombre: "Franco Di Leo",
+};
+const alumnos = [
+  {
+    id: 1,
+    nombre: "Fran",
+    apellido: "Di Leo",
+    edad: 26,
+  },
+  {
+    id: 2,
+    nombre: "Pablo",
+    apellido: "Banfi",
+    edad: 39,
+  },
+];
+router.get("/", (req, res) => {
+  // devuelve una vista
+  res.render("alumnos", {
+    alumnos: alumnos,
+    profe: profe,
+    curso: "Programador web inicial",
+  });
+});
+
+module.exports = router;
